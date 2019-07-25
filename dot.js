95c12e483bfd6fe41cee91da1de2e2997bc13ec7
Max Iliv <max.r.iliv@gmail.com>
2019-07-25 20:00:27 +0300

    lol

--- dot.js
+++ dot.js
@@ -2,7 +2,13 @@ function Dot(x, y) {
     this.x = x;
     this.y = y;
     this.r = Math.random() * size / 2;
-    this.factor = 0.05;
+    this.factor = 1;
+    this.velocity = {
+        x: Math.random() > 0.5 ? 5 : -5,
+        y: Math.random() > 0.5 ? 5 : -5
+    };
+    this.escapeMode = false;
+    this.grow = false;
 };
 
 Dot.prototype.draw = function () {
@@ -10,21 +16,52 @@ Dot.prototype.draw = function () {
     ctx.save();
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-    ctx.fillStyle = this.fill;
+    ctx.fillStyle = this.fill || '#555';
     ctx.fill();
     ctx.stroke();
     ctx.restore();
 }
 
+Dot.prototype.escape = function () {
+    if (this.x + this.r > c.width) {
+        this.velocity.x = -(this.velocity.x);
+    }
+
+    if (this.x - this.r < 0) {
+        this.velocity.x = -(this.velocity.x);
+    }
+
+    if (this.r + this.y > c.height) {
+        this.velocity.y = -(this.velocity.y);
+    }
+
+    if (this.y - this.r < 0) {
+        this.velocity.y = -(this.velocity.y);
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+};
+
 Dot.prototype.update = function () {
+    if (this.escapeMode) {
+        this.escape();
+    }
+
+    if (this.grow) {
+        this.changeRadius();
+    }
+};
+
+Dot.prototype.changeRadius = function () {
     this.r += this.factor;
 
     if (this.r > size / 2) {
-        this.factor = -0.1;
+        this.factor = -1;
     }
 
     if (this.r < size / 10) {
-        this.factor = 0.1;
+        this.factor = 1;
     }
 }
 
@@ -37,7 +74,7 @@ Dot.prototype.colorize = function () {
 }
 
 Dot.prototype.unColorize = function () {
-    this.fill = 'black';
+    this.fill = 'white';
 }
 
 Dot.prototype.getRandomColor = function () {
@@ -48,4 +85,5 @@ Dot.prototype.colors = ['#fea49f',
 '#fbaf08', '#091f36', '#c7af6b', '#e4decd',
 '#c2dde6', '#bccbde', '#8bf0ba', '#0e0fed',
 '#ffdc6a', '#e62739', '#ed8a63', '#845007',
-'#d4d4dc', '#393f4d'];
+'#d4d4dc', '#393f4d', '#C9D6EA', '#DC6BAD',
+'#B3C0A4', '#E9E3E6', '#815355'];
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var height = 500;
-var width = 500;
-var size = 50;
+var size = Math.random() * 120 + 30;
+var height = Math.floor(window.innerHeight / size) * size;
+var width = Math.floor(window.innerWidth / size) * size;
 var dots = [];
 
 var c = document.getElementById('canvas');
@@ -10,11 +10,11 @@ function setup () {
     c.width = width;
     c.height = height;
     createDots();
-    subscribeCanvasevents();
+    subscribeCanvasEvents();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, c.width, c.height);
+    background();
     // drawGrid();
     drawDots();
     window.requestAnimationFrame(draw);
@@ -27,7 +27,7 @@ function randomColor () {
 
 function createDots () {
     for (let i = 0; i < width; i+=size) {
-        for (let j = 0; j < width; j+=size) {
+        for (let j = 0; j < height; j+=size) {
             var x = i + (size / 2);
             var y = j + (size / 2);
             dots.push(new Dot(x, y));
@@ -35,26 +35,43 @@ function createDots () {
     }
 }
 
-function subscribeCanvasevents () {
-    c.addEventListener('mousemove', checkHit);
+function subscribeCanvasEvents () {
+    c.addEventListener('mousemove', tryColorize);
+    c.addEventListener('click', removeDot);
 }
 
-function checkHit (e) {
+function checkHit (e, d) {
     var x = e.clientX;
     var y = e.clientY;
 
-    dots.forEach(d => {
-        var dx = d.x - x;
-        var dy = d.y - y;
+    return dist(d.x, d.y, x, y) <= d.r;
+}
 
-
-        var dist = Math.sqrt(dx * dx + dy * dy);
-        if (
-            dist <= d.r
-        ) {
-            d.colorize();
+function tryColorize(e) {
+    for (var i = 0; i < dots.length; i++) {
+        if (checkHit(e, dots[i])) {
+            dots[i].colorize();
+            dots[i].escapeMode = true;
+            dots[i].grow = true;
+            break;
         }
-    });
+    }
+}
+
+function dist (x, y, x1, y1) {
+    var dx = x - x1;
+    var dy = y - y1;
+
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function removeDot (e) {
+    for (var i = 0; i < dots.length; i++) {
+        if (checkHit(e, dots[i])) {
+            dots.splice(i, 1);
+            break;
+        }
+    }
 } 
 function drawGrid() {
     ctx.beginPath();
@@ -74,6 +91,14 @@ function drawGrid() {
 
 function drawDots () {
     dots.forEach(d => d.draw());
+}
+
+function background (color) {
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.save();
+    ctx.fillStyle = color || '#333';
+    ctx.fillRect(0, 0, c.width, c.height)
+    ctx.restore();
 }
 
 setup();
